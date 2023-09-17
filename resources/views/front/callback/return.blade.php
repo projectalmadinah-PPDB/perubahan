238fcedf3ee08c6b1e22c1e5358e57539dd67873
haidaryuum <120004849+yuumuu@users.noreply.github.com>
2023-09-17 10:53:52 +0700

    sedikit perubahan di greeting payment

--- resources/views/front/callback/return.blade.php
+++ resources/views/front/callback/return.blade.php
@@ -5,22 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css"  rel="stylesheet" />
     <title>Terimakasih</title>
+    @vite(['resources/css/real.css'])
+    @include('front.layouts.include')
+    @stack('css')
 </head>
 <body>
     
-    <div class="my-10">
-        <figure class="max-w-screen-md h-screen mx-auto text-center">
-            <img class="rounded-full w-96 h-96 mx-auto mb-5" src="ty.jpeg" alt="image description">
-            <svg class="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+    <div class="w-full h-auto bg-gradient-to-t from-dasar to-sky-100">
+        <figure class="max-w-screen-md h-screen mx-auto text-center flex flex-col justify-center items-center bg-gradient-to-b from-dasar to-sky-100">
+            <img class="w-80 h-80 mb-12" src="/dists/images/logo_only.svg" alt="image description">
+            <svg class="w-10 h-10 mb-3 text-slate-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
                 <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
             </svg>
-            <blockquote>
-                <p class="text-2xl italic font-medium text-gray-900 dark:text-white">"Pembayaran Berhasil!!<br>Terimakasih."</p>
-            </blockquote>
+            <p class="text-2xl italic font-medium text-slate-900">
+                "Pembayaran Berhasil<br>dilakukan!!<br>Terimakasih."
+            </p>
             <figcaption class="flex items-center justify-center mt-6 space-x-3">
-                <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                    <a href="{{route('user.dashboard')}}" class="pl-3 text-sm text-gray-500 dark:text-gray-400">Kembali Ke Home</a>
-                    <a class="pl-3 text-sm text-gray-500 dark:text-gray-400">Website E-Learning</a>
+                <div class="flex items-center divide-x-2 divide-slate-500">
+                    <a href="{{route('user.dashboard')}}" class="px-3 text-sm text-slate-500 hover:text-slate-800"
+                    >Kembali ke <span class="font-semibold">Dashboard</span></a>
+                    <a href="{{route('front')}}" class="px-3 text-sm text-slate-500 font-semibold hover:text-slate-800 uppercase">Home</a>
                 </div>
             </figcaption>
         </figure>
