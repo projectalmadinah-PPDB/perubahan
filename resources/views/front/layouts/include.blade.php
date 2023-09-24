ed31f121a08505f6a0278b168ca4bf77567a23f9
farhan181206 <farhanmuti54@gmail.com>
2023-09-24 20:03:47 +0700

    Committing local changes to include.blade.php

--- resources/views/front/layouts/include.blade.php
+++ resources/views/front/layouts/include.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="resources/css/final.css">
+<link rel="stylesheet" href="resources/css/real.css">
 <script src="https://cdn.tailwindcss.com"></script>
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
@@ -14,5 +14,6 @@
 <!-- swiper js -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
 
-<link rel="stylesheet" href="/dists/assets/style.css"/>
+{{-- <link rel="stylesheet" href="./dists/fots/assets/style.css"/> --}}
+<link rel="stylesheet" href="./dists/assets/style.css"/>
 <link rel="stylesheet" href="resources/css/output.css"/>
